95154c726c216070c0d8732c0a4d05768429bf70
jonathancaudill <jsccjr1@gmail.com>
2025-08-01 18:10:59 -0400

    Fix draggable window view, bridging from appkit view

---
+++ Pulse/Utils/DragWindowView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+import AppKit
+
+struct DragWindowView: NSViewRepresentable {
+    func makeNSView(context: Context) -> NSView {
+        let view = DraggableNSView()
+        return view
+    }
+
+    func updateNSView(_ nsView: NSView, context: Context) {
+        // No update needed
+    }
+}
+
+class DraggableNSView: NSView {
+    override func mouseDown(with event: NSEvent) {
+        if let window = self.window {
+            window.performDrag(with: event)
+        }
+    }
+}
