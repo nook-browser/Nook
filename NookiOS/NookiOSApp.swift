505430ff5f29097816b1fc5319554f1412629a5f
Bain Gurley <baingurley@gmail.com>
2026-09-18 13:35:34 -0500

    ios: NookiOS app target

---
+++ NookiOS/NookiOSApp.swift
@@ -0,0 +1,9 @@
+// Licensed under GPL-3.0 with the App Store exception in LICENSE-EXCEPTION.md.
+import SwiftUI
+
+@main
+struct NookiOSApp: App {
+    var body: some Scene {
+        WindowGroup { Text("Nook") }
+    }
+}
